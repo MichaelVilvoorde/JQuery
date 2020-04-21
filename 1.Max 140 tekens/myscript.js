b8e7ad261592652c22b791e392da953334050021
MichaelVilvoorde <baeyensmichael@hotmail.com>
2020-04-21 14:24:21 +0200

    .

--- 1.Max 140 tekens/myscript.js
+++ 1.Max 140 tekens/myscript.js
@@ -1,17 +1,10 @@
 $(function() {
-    $('h1').css('color', 'red');
-
     var maxLength = 140;
     $('#input').keyup(function() {
         var length = maxLength - $(this).val().length + ' Character(s) Remaining';
         $('#rest').text(length);
     });
-
-
-
-
-    // Schrijf een functie die de input .val() van een veld controleert en maximaal 140 tekens
-    // toelaat.
-    // Tip gebruik: keyup() en .text()
-
 });
+// Schrijf een functie die de input .val() van een veld controleert en maximaal 140 tekens
+// toelaat.
+// Tip gebruik: keyup() en .text()
